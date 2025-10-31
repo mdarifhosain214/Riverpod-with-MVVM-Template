af0a700262a5c3f33844c70b439c70965d4b160b
Arif <mdarifhosain214@gmail.com>
2025-10-31 16:41:00 +0600

    updated

--- lib/core/route/route_manager.dart
+++ lib/core/route/route_manager.dart
@@ -7,7 +7,6 @@ class Routes{
   static const String loginRoute="/login";
   static const String registerRoute="/register";
   static const String forgotPasswordRoute="/forgotPassword";
-  static const String mainRoute="/main";
   static const String storeDetailsRoute="/storeDetails";
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_structure/core/route/route_config.dart';
+import 'package:flutter_app_structure/core/route/route_manager.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
