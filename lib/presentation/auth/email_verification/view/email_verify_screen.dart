d97d515695c49fb153d64e43da3f010e30643029
Arif <mdarifhosain214@gmail.com>
2026-04-06 16:45:40 +0600

    updated

---
+++ lib/presentation/auth/email_verification/view/email_verify_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmailVerification extends StatefulWidget {
+  const EmailVerification({super.key});
+
+  @override
+  State<EmailVerification> createState() => _EmailVerificationState();
+}
+
+class _EmailVerificationState extends State<EmailVerification> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
