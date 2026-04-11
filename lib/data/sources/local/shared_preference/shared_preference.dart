48675979f9451b379565935298bfa03f23f7c6f2
mdarifhosain214 <mdarifhosain214@gmail.com>
2026-04-11 10:24:05 +0600

    updated share Preference file

--- lib/data/sources/local/shared_preference/shared_preference.dart
+++ lib/data/sources/local/shared_preference/shared_preference.dart
@@ -1,49 +1,53 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+const String _authToken = 'auth_token';
+const String _role = 'role';
+const String _email = 'email';
+
 class SharedPreferenceData {
-static Future<void> setToken(String? token) async {
+  static Future<void> setToken(String? token) async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.setString('auth_token', "$token");
+    prefs.setString(_authToken, "$token");
   }
 
   static Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('auth_token');
+    return prefs.getString(_authToken);
   }
 
   static Future<void> removeToken() async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.remove('auth_token');
+    prefs.remove(_authToken);
   }
 
   static Future<String?> getRole() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('role');
+    return prefs.getString(_role);
   }
 
   Future<void> setRole(String? role) async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.setString('role', "$role");
+    prefs.setString(_role, "$role");
   }
 
   static Future<void> removeRole() async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.remove('role');
+    prefs.remove(_role);
   }
 
   // EMAIL
   Future<String?> getEmailId() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('email');
+    return prefs.getString(_email);
   }
 
   Future<void> setEmailId(String? id) async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.setString('email', id ?? "No email_id");
+    prefs.setString(_email, id ?? "No email_id");
   }
 
   Future<void> removeEmailId() async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.remove('email');
+    prefs.remove(_email);
   }
 }
